335fd6b8b8cb49da3d8491d3a1c35206ecbda4fc
Shifter447 <tbesdemiotis@gmail.com>
2025-05-14 12:31:26 +0200

    movement
    added basic movement, crouching and jumping.

---
+++ Assets/scripts/movement/Movement.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 10f; // Movement speed
+    public float crouchSpeed = 4f; // Crouch movement speed
+    public float turnSpeed = 8f; // Speed of turning
+    public Camera playerCamera; // Camera to determine direction
+    public float standingHeight = 2f;
+    public float crouchingHeight = 1f;
+    public KeyCode crouchKey = KeyCode.LeftControl;
+
+    private float cameraPitch = 0f; // Camera pitch angle
+    [SerializeField] private bool isCrouching = false;
+    private float originalCameraY;
+
+    void Start()
+    {
+        originalCameraY = playerCamera.transform.localPosition.y;
+    }
+
+    void Update()
+    {
+        HandleMouseLook();
+        HandleMovement();
+        HandleCrouch();
+    }
+
+    void HandleMouseLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * turnSpeed;
+        float mouseY = Input.GetAxis("Mouse Y") * turnSpeed;
+
+        // Rotate character left/right (yaw)
+        transform.Rotate(Vector3.up * mouseX);
+
+        // Rotate camera up/down (pitch)
+        cameraPitch -= mouseY;
+        cameraPitch = Mathf.Clamp(cameraPitch, -80f, 80f);
+        playerCamera.transform.localEulerAngles = new Vector3(cameraPitch, 0f, 0f);
+    }
+
+    void HandleMovement()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.forward * moveZ + transform.right * moveX;
+        move.Normalize();
+
+        float currentSpeed = isCrouching ? crouchSpeed : moveSpeed;
+        transform.position += move * currentSpeed * Time.deltaTime;
+    }
+
+    void HandleCrouch()
+    {
+        if (Input.GetKeyDown(crouchKey))
+        {
+            isCrouching = true;
+
+            // Lower the camera
+            Vector3 camPos = playerCamera.transform.localPosition;
+            camPos.y = originalCameraY - 0.5f;
+            playerCamera.transform.localPosition = camPos;
+
+            // Scale player down
+            Vector3 scale = transform.localScale;
+            scale.y = crouchingHeight / standingHeight;
+            transform.localScale = scale;
+        }
+        else if (Input.GetKeyUp(crouchKey))
+        {
+            // Check for obstruction above the player before standing
+            float headClearance = (standingHeight - crouchingHeight) * 0.5f;
+            Vector3 rayOrigin = transform.position + Vector3.up * (crouchingHeight * 0.5f);
+            bool isBlocked = Physics.Raycast(rayOrigin, Vector3.up, headClearance);
+
+            if (!isBlocked)
+            {
+                isCrouching = false;
+
+                // Reset camera height
+                Vector3 camPos = playerCamera.transform.localPosition;
+                camPos.y = originalCameraY;
+                playerCamera.transform.localPosition = camPos;
+
+                // Reset scale
+                Vector3 scale = transform.localScale;
+                scale.y = 1f;
+                transform.localScale = scale;
+            }
+            else
+            {
+                // Optional: debug message
+                Debug.Log("Can't stand up! Obstruction above.");
+            }
+        }
+    }
+}
